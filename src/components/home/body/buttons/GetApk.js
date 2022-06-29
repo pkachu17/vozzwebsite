aab6a3f7e1a270b0aae49df3f1c0442d95ab3c97
Prashant Jha <user.name>
2022-06-29 02:50:40 -0400

    vozzwebsite

--- src/components/home/body/buttons/GetApk.js
+++ src/components/home/body/buttons/GetApk.js
@@ -6,7 +6,7 @@ const GetApk = () => {
     return (
         <div><Header text="Get Apk" />
             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <img src={deviceImg} style={{ width: '400px' }}></img>
+                <img src={deviceImg} style={{ height:'400px' }}></img>
                 <button type="button" class="btn btn-success">Download for Android</button>
             </div>
         </div>
